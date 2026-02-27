398406ee7f66fff44774da56d3dfd684b433365d
SultonUzDev <sultonuzdev@gmail.com>
2026-02-27 17:50:37 +0500

    compose code clean up

--- app/src/main/java/com/sultonuzdev/andersentask/MainActivity.kt
+++ app/src/main/java/com/sultonuzdev/andersentask/MainActivity.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import com.sultonuzdev.andersentask.presentation.ProductScreen
 import com.sultonuzdev.andersentask.core.ui.theme.AndersenTaskTheme
 
 class MainActivity : ComponentActivity() {
@@ -13,7 +12,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             AndersenTaskTheme {
-                ProductScreen()
+
             }
         }
     }
